bff5bc584a1cd73ab74e90904abe1d59b359bd33
wo7864 <33424923+wo7864@users.noreply.github.com>
2023-02-16 17:58:54 +0900

    update Key

--- src/routes/v-keyboard/Key.svelte
+++ src/routes/v-keyboard/Key.svelte
@@ -1,16 +1,22 @@
 <script>
+    export let width = 40;
     export let value;
     export let typingText;
 
-    const handleClick = () => typingText(value)
+    export let handleClick = () => typingText(value)
 
 </script>
 
-<button on:click={handleClick}>
+<button on:click={handleClick}  style="--width: {width}px;">
 {value}
 </button>
 
 
 <style>
-
+button{
+    width: var(--width);
+    height:40px;
+    margin:0;
+    padding:0;
+}
 </style>
--- src/routes/v-keyboard/VKeyboard.svelte
+++ src/routes/v-keyboard/VKeyboard.svelte
@@ -1,17 +1,16 @@
 
 <script>
     import Key from "./Key.svelte";
-
     let language = 'lower'
     let text=''
     let dict = {'lower':[
-            '1234567890\-\=',
+            '`1234567890\-\=',
             'qwertyuiop\[\]',
             'asdfghjkl\;\'',
             'zxcvbnm\,\.\/',
         ].map(item => item.split('')),
         'upper':[
-            '!@#$%^&*()_+',
+            '~!@#$%^&*()_+',
             'QWERTYUIOP\{\}',
             'ASDFGHJKL\:\"',
             'ZXCVBNM\<\>\?'
@@ -20,21 +19,28 @@
     
     }
     const typingText = (v) => text += v
+    const shiftClick = () => language === 'lower' ? language = 'upper': language='lower'
     
 </script>
 
 <div>
     <p>아래에 입력됩니다</p>
-    <textarea readonly bind:value={text}></textarea>
+    <textarea on:keydown|preventDefault bind:value={text}></textarea>
 </div>
 
 {#each dict[language] as keyLine, i}
 
-    <div>
-        {#each Array(i) as j} &nbsp;{/each}
+    <div class="line">
+        <!-- {#each Array(i) as j} &nbsp;&nbsp;&nbsp;{/each} -->
+        {#if i ==1}<Key width={60} value=" "/>
+        {:else if i ==2}<Key width={80} value="Shift" handleClick={shiftClick}/>
+        {:else if i ==3}<Key width={100} value=""/>{/if}
+
         {#each keyLine as keyValue}
             <Key value={keyValue} {typingText}/>
         {/each}
+        {#if i ==0}<Key width={60} value="←"/>{/if}
+
     </div>  
 {/each}
 
@@ -42,5 +48,10 @@
     textarea{
         width:400px;
         height:400px;
+    }
+
+    .line {
+        display: flex;;
+        margin-bottom:5px;
     }
 </style>
